c2bfe52af4be1da3d3e6d878fdad8edc818fdf52
x-color <36035885+x-color@users.noreply.github.com>
2022-07-31 18:44:18 +0900

    Add debug

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,8 @@ async function run(): Promise<void> {
   try {
     const mdFile = core.getInput('file')
     const vars = yaml.load(core.getInput('vars')) as Map<string, string>
+    // eslint-disable-next-line no-console
+    console.log(vars)
     const md = await fs.readFile(mdFile)
     await core.summary.addRaw(format(md.toString(), vars)).write()
   } catch (error) {
